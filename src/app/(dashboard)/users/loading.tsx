868dd4b119b4df3978aaa6d3d42e6dd48aadeb7e
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-10-28 21:32:17 +0700

    feat: update loading skeletons with background color for improved visibility

--- src/app/(dashboard)/users/loading.tsx
+++ src/app/(dashboard)/users/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function Loading() {
   return (
     <div className='w-full'>
-      <Skeleton className='h-1 w-full rounded-full' />
+      <Skeleton className='h-1 w-full rounded-full bg-sky-500' />
     </div>
   );
 }
