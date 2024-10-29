c8dad9b55aa83a1b0957ad30d07434b3383d1e20
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-10-29 21:04:48 +0700

    feat: update Kysely integration for PostgreSQL with new connection settings and remove deprecated package

--- src/lib/kysely.ts
+++ src/lib/kysely.ts
@@ -1,7 +1,17 @@
 import { DB } from '@/types/db';
-import { createKysely } from '@vercel/postgres-kysely';
+import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
 
-export const db = createKysely<DB>({
-  connectionString: process.env.POSTGRES_URL,
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5434,
+    max: 10,
+  }),
 });
+
+export const db = new Kysely<DB>({ dialect });
 export { sql } from 'kysely';
